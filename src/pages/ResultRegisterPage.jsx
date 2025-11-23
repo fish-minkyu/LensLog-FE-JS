e0bea23d43fbeb68cdd46542fe2452fdf50487a6
fish-minkyu <e951219@naver.com>
2025-11-23 14:17:52 +0900

    Feat. 회원가입 완료 화면 page 완료

--- src/pages/ResultRegisterPage.jsx
+++ src/pages/ResultRegisterPage.jsx
@@ -1,5 +1,29 @@
+import "../css/ResultRegisterPage.css";
+import LogoHeader from "../components/LogoHeader";
+import happyFace from "../assets/happy-face.svg";
+import { useNavigate } from "react-router-dom";
+
 const ResultRegisterPage = () => {
-    return <div>회원가입 결과 화면</div>;
+    const nav = useNavigate();
+
+    return (
+        <div className="ResultRegisterPage">
+            <LogoHeader />
+            <img className="happy-face" src={happyFace} alt="happy-face" />
+            <div className="result-text">
+                <h3>회원가입이 완료되었습니다.</h3>
+                <h3>로그인 해주세요!</h3>
+            </div>
+            <button
+                className="login-page-button"
+                onClick={() => {
+                    nav("/login");
+                }}
+            >
+                로그인 화면으로
+            </button>
+        </div>
+    );
 };
 
 export default ResultRegisterPage;
